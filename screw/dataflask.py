e8bedd4ddad7fefebce8368b9cd422971d8b8d38
silverbaum <39158562+TaxiDriver202@users.noreply.github.com>
2025-01-01 18:41:58 +0200

    Added flask SocketIO to refresh the data

--- screw/dataflask.py
+++ screw/dataflask.py
@@ -1,38 +1,63 @@
 import asyncio
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_socketio import SocketIO, emit
 import os
-#import matplotlib.pyplot as plt
-#import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as pd
 import threading
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-kitchen= {
+kitchen = {
     "temp": 0,
     "humidity": 0,
-    "pressure": 0
+    "pressure": 0,
+    "acceleration_x": 0,
+    "acceleration_y": 0,
+    "acceleration_z": 0
 }
 
 outside = {
     "temp": 0,
     "humidity": 0,
-    "pressure": 0
+    "pressure": 0,
+    "acceleration_x": 0,
+    "acceleration_y": 0,
+    "acceleration_z": 0
 }
-
-#todo: labels corrected
 
 async def collect_data():
     async for found_data in RuuviTagSensor.get_data_async():
-       # print(f"MAC: {found_data[0]}")
+        data_updated = False
         if(found_data[0]=="DD:60:98:83:37:20"):
-            kitchen.update({'temp': found_data[1]["temperature"], 'humidity': found_data[1]["humidity"], 'pressure': found_data[1]["pressure"]})
+            kitchen.update({
+                'temp': found_data[1]["temperature"], 
+                'humidity': found_data[1]["humidity"], 
+                'pressure': found_data[1]["pressure"],
+                'acceleration_x': found_data[1]["acceleration_x"],
+                'acceleration_y': found_data[1]["acceleration_y"],
+                'acceleration_z': found_data[1]["acceleration_z"]
+            })
+            data_updated = True
         elif(found_data[0]=="D6:34:9B:BF:40:B2"):
-            outside.update({'temp': found_data[1]["temperature"], 'humidity': found_data[1]["humidity"], 'pressure': found_data[1]["pressure"]})
-
-       # print(f"Data: {found_data[1]['temperature']}")
-       # print(f"Kitchen: {kitchendata['temp']}")
-       # print(f"Outside: {outside['temp1']}")
+            outside.update({
+                'temp': found_data[1]["temperature"], 
+                'humidity': found_data[1]["humidity"], 
+                'pressure': found_data[1]["pressure"],
+                'acceleration_x': found_data[1]["acceleration_x"],
+                'acceleration_y': found_data[1]["acceleration_y"],
+                'acceleration_z': found_data[1]["acceleration_z"]
+            })
+            data_updated = True
+        
+        # Emit updates only when data changes
+        if data_updated:
+            socketio.emit('data_update', {
+                "kitchen": kitchen,
+                "outside": outside
+            })
         
 @app.route('/')
 def dashboard(): 
@@ -49,5 +74,6 @@ def start_data_collection():
 
 if __name__ == "__main__":
     data_thread = threading.Thread(target=start_data_collection)
+    data_thread.daemon = True  # This ensures the thread will exit when the main program exits
     data_thread.start()
-    app.run(debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
